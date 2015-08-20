91be81bb91275536d646630774b5d579c92d6838
KeleiAzz <kgong@ncsu.edu>
2015-08-19 21:18:02 -0400

    Reference a discussion

--- Maximum Subarray.py
+++ Maximum Subarray.py
@@ -1,10 +1,12 @@
-__author__ = 'keleigong'
 class Solution:
     # @param {integer[]} nums
     # @return {integer}
     def maxSubArray(self, nums):
-        # if len(nums) == 2:
-        #     return max(max(nums), sum(nums))
-        row = [0] * len(nums)
-        res = [row] * len(nums)
-
+        a = [0]*len(nums)
+        a[0] = nums[0]
+        res = a[0]
+        for i in range(1,len(nums)):
+            a[i] = max(a[i-1] + nums[i], nums[i])
+            res = max(res, a[i])
+                
+        return res
